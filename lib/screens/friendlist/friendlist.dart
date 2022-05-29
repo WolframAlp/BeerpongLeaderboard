ece1507a29253af42b20cb485eb76ac0a4d48aaf
WolframAlp <47143558+WolframAlp@users.noreply.github.com>
2022-05-30 01:37:10 +0200

    added empty text and icon
    also imported user and friendlist by provider of last loaded user

--- lib/screens/friendlist/friendlist.dart
+++ lib/screens/friendlist/friendlist.dart
@@ -4,8 +4,8 @@
 
 // friend suggestions??
 
-import 'package:beerpong_leaderboard/services/page_manager.dart';
 import 'package:beerpong_leaderboard/utilities/constants.dart';
+import 'package:beerpong_leaderboard/utilities/user.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:beerpong_leaderboard/services/auth.dart';
@@ -14,34 +14,68 @@ class FriendList extends StatelessWidget {
   final AuthService _auth = AuthService();
   FriendList({Key? key}) : super(key: key);
 
+  Widget _getEmpty() {
+    return Center(
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 50.0,
+            width: 300.0,
+          ),
+          Text(
+            "WoW",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 50,
+            ),
+          ),
+          SizedBox(
+            height: 50.0,
+            width: 300.0,
+          ),
+          Text(
+            "Such Empty",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 50,
+            ),
+          )
+        ],
+        crossAxisAlignment: CrossAxisAlignment.center,
+      ),
+    );
+  }
+
+  Widget _getFull() {
+    return Container();
+  }
+
   @override
   Widget build(BuildContext context) {
+    UserModel user = context.read<LastUserLoad>().lastLoad;
+    List friends = user.friends;
+
+    Widget mainWidget = friends.isEmpty ? _getEmpty() : _getFull();
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue[300],
         title: const Text(
-          "FriendList or at lease will be",
+          "Friends are the best",
           style: kHintTextStyle,
         ),
         elevation: 0.0,
         actions: <Widget>[
           TextButton.icon(
-            onPressed: () {
-              // Navigator.pop(context);
-              context.read<PageManager>().goToUsername;
-            },
-            icon: const Icon(Icons.verified_user),
-            label: const Text("Intro"),
-          ),
-          TextButton.icon(
               onPressed: () async {
                 await _auth.signOut();
                 Navigator.pop(context);
               },
-              icon: const Icon(Icons.logout),
-              label: const Text("Logout")),
+              icon: const Icon(Icons.person_add_alt),
+              label: const Text("Add")),
         ],
       ),
+      body: mainWidget,
     );
   }
 }
