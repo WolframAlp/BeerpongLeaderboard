61934404cbaa62730ec6db123c1313da25e806df
WolframAlp <47143558+WolframAlp@users.noreply.github.com>
2022-05-29 14:12:16 +0200

    added some database functions for leaderboard

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -15,6 +15,9 @@ class DatabaseService {
   final CollectionReference trophysCollection =
       FirebaseFirestore.instance.collection('trophys');
 
+  final CollectionReference leaderboardCollection = 
+      FirebaseFirestore.instance.collection('leaderboard');
+
   // Create new user in database
   Future createNewUser() async {
     return await userCollection.doc(name).set({
@@ -24,35 +27,47 @@ class DatabaseService {
       'games': 0,
       'elo': 1000,
       'friends': [],
-    });
+    }, SetOptions(merge: true));
   }
 
   // Create new database entry in trophies
   Future createtrophyUser() async {
     return await trophysCollection.doc(name).set({
       "CoolName": FieldValue.serverTimestamp(),
-    });
+    }, SetOptions(merge: true));
   }
 
   // Add a new trophy to the current user
   Future addtrophyToUser(String trophyName) async {
-    return await trophysCollection
-        .doc(name)
-        .set({trophyName: FieldValue.serverTimestamp()});
+    return await trophysCollection.doc(name).set(
+        {trophyName: FieldValue.serverTimestamp()},
+        SetOptions(merge: true)); // This case merge, because we add a new value
   }
 
   Future updateUserWinsAndGames() async {
-    DocumentSnapshot document = await userCollection.doc(name).get();
-    return await userCollection.doc(name).set({
-      "games": document.get("games") + 1,
-      "wins": document.get("wins") + 1,
-    });
+    return await userCollection.doc(name).update({
+      "games": FieldValue.increment(1), // Adds 1 to the value in the database (can only be called once a second)
+      "wins": FieldValue.increment(1),
+    }); // This case use update because existing values are overwritten
   }
 
   Future updateUserGames() async {
-    DocumentSnapshot document = await userCollection.doc(name).get();
-    return await userCollection.doc(name).set({
-      "games": document.get("games") + 1,
+    return await userCollection.doc(name).update({
+      "games": FieldValue.increment(1),
+    }); // This case use update because existing values are overwritten
+  }
+
+  // Create new database entry in leaderboard
+  Future createrUserOnLeaderboard() async {
+    return await leaderboardCollection.doc(name).set({
+      "elo": 1000,
+    }, SetOptions(merge: true));
+  }
+
+  // Update the elo of user on leaderboard
+  Future updateUserOnLeaderboard(int newValue) async {
+    return await leaderboardCollection.doc(name).update({
+      "elo": newValue,
     });
   }
 
@@ -80,6 +95,12 @@ class DatabaseService {
     return trophysCollection.doc(name).snapshots().map(_trophysFromSnapshot);
   }
 
+  // TODO mapping in the streams could be done automatically from snapshots using the firebase build in costum objects methods : https://firebase.google.com/docs/firestore/manage-data/add-data#custom_objects
+
+  // TODO what happens when offline ??, will information be updated later or never? what if you accept a game, is your elo updated, or do you need to accept it again later?
+
+  // TODO for offline users it might be usefull to use the cache option : https://firebase.google.com/docs/firestore/query-data/get-data#source_options
+
   // Single user's trophies from document snapshot
   TrophyModel _trophysFromSnapshot(DocumentSnapshot snapshot) {
     List<String> trophyNames = [];
@@ -89,9 +110,12 @@ class DatabaseService {
     for (var key in data.keys) {
       trophyNames.add(key);
       trophyTimes.add(data[key]);
-      trophyMap.add({key:data[key]});
+      trophyMap.add({key: data[key]});
     }
-    return TrophyModel(trophyNames: trophyNames, trophyTimes: trophyTimes, trophyMap: trophyMap);
+    return TrophyModel(
+        trophyNames: trophyNames,
+        trophyTimes: trophyTimes,
+        trophyMap: trophyMap);
   }
 
   // Single user from document snapshot
